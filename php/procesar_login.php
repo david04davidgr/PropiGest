77b3a62d47d9daa2eee74243a30c2bcaf6679ad2
david04davidgr <davidgarcia.r.04@gmail.com>
2025-06-09 20:11:25 +0200

    fixed CORS

--- php/procesar_login.php
+++ php/procesar_login.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
 header('Content-Type: application/json');
 include 'conexion.php';
 
